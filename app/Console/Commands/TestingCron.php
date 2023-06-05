c7803111b0fc26d3e9303be478c40da14ee7ce7e
unknown <abooddablo@gmail.com>
2023-06-05 15:12:58 +0300

    first commit

---
+++ app/Console/Commands/TestingCron.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestingCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:testing-cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CommandController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommandController extends Controller
+{
+public function makeCommand(){
+
+
+
+    
+}
+}
